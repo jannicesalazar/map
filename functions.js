fd817a60c0162fd50cbca446ff65b47aa38c8c2d
jannicesalazar <jannicesalazars@gmail.com>
2023-11-27 12:49:04 -0500

    ji

--- functions.js
+++ functions.js
@@ -1,41 +1,37 @@
-// Initialize the map
+// initialize the map
 var map = L.map('map').setView([0, 0], 2);
 
-// Add a tile layer (you can use other tile layers as well)
+// add a tile layer 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
-}).addTo(map);
+    attribution: '© OpenStreetMap contributors'}).addTo(map);
 
-//Event handler for when the map is clicked 
+// event handler for when map is clicked 
 function onMapClick(e) {
-    // Fetch weather data using OpenWeatherMap API
+    // fetch weather data with OpenWeatherMap API
     const apiKey = '6dea368c99a053969745d5e110936277';
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${e.latlng.lat}&lon=${e.latlng.lng}&appid=${apiKey}&units=metric`;
 
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            // Extract relevant weather information
+            // extract weather information
             const weatherDescription = data.weather[0].description;
             const temperature = data.main.temp;
+            const temperatureF = (temperature *9/5) + 32;
 
-            // Create a popup with weather information
+            // creates a popup with weather information
             const popupContent = `
                 <b>Latitude:</b> ${e.latlng.lat}<br>
                 <b>Longitude:</b> ${e.latlng.lng}<br>
                 <b>Weather:</b> ${weatherDescription}<br>
-                <b>Temperature:</b> ${temperature}°C
-            `;
+                <b>Temperature:</b> ${temperature}°C / ${temperatureF}°F`;
 
-            // Add a marker with the popup to the clicked location
+            // add a marker with the popup to the clicked location
             L.marker(e.latlng).addTo(map)
                 .bindPopup(popupContent)
-                .openPopup();
-        })
-        .catch(error => {
-            console.error('Error fetching weather data:', error);
-        });
-}
+                .openPopup();})
+        
+        .catch(error => {console.error('Error fetching weather data:', error);}); }
 
-// Attach the click event handler to the map
+// attach click event handler to the map
 map.on('click', onMapClick);
