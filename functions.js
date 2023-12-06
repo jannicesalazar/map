5d431abfe1b91458697645a37a03ccf7ccd62827
jannicesalazar <jannicesalazars@gmail.com>
2023-12-06 12:21:02 -0500

    hi

--- functions.js
+++ functions.js
@@ -1,12 +1,13 @@
 'use strict';
 
-const apiKey = '6dea368c99a053969745d5e110936277';
-const map = L.map('map').setView([0, 0], 2);
+const apiKey = '6dea368c99a053969745d5e110936277'; //API key for accessing OpenWeather Map
+const map = L.map('map').setView([0, 0], 2); //leaflet map centered at [0,0] with 2 level zoom 
 let currentMarker;
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: 'Map' }).addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: 'Map' }).addTo(map); //OpenStreetMap tile layer on map 
 
-const showWelcomePopup = () => {
+//shows welcome popup only when visiting site for the first time 
+const welcomePopup = () => {
     const hasVisited = localStorage.getItem('hasVisited');
     if (!hasVisited) {
         Swal.fire({
@@ -16,94 +17,97 @@ const showWelcomePopup = () => {
             confirmButtonText: 'Have Fun!',
             confirmButtonColor: '#00a5ff',
         });
-        localStorage.setItem('hasVisited', 'true');
-    }
-};
+        //flags user when they have visited 
+        localStorage.setItem('hasVisited', 'true'); } };
 
-const fetchWeatherData = (lat, lng) => {
+//gets data from OpenWeaterMap API
+const weatherData = (lat, lng) => {
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${apiKey}&units=metric`;
 
     return fetch(apiUrl)
         .then(response => response.json())
         .catch(error => {
             console.error('Error fetching weather data:', error);
-            throw error;
-        });
-};
+            throw error; }); };
 
-const createPopupContent = (data, lat, lng) => {
+//creates a popup with data from the weather data
+const dataPopup = (data, lat, lng) => {
     const weatherDescription = data.weather[0].description;
     const temperature = data.main.temp;
     const temperatureF = (temperature * 9/5) + 32;
     const iconCode = data.weather[0].icon;
     const weatherIconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
 
-    return `
-        <div class="popup-container">
+    return `<div class="popup-container">
             ${weatherDescription.toUpperCase()}<br>
             <img src="${weatherIconUrl}" alt="Weather Icon" class="weather-icon"><br>
             ${temperature.toFixed(2)}°C / ${temperatureF.toFixed(2)}°F <br>
             <b>Latitude:</b> ${lat.toFixed(2)}<br>
-            <b>Longitude:</b> ${lng.toFixed(2)}<br>
-        </div>`;
-};
+            <b>Longitude:</b> ${lng.toFixed(2)}<br></div>`; };
 
-const addMarkerToMap = (latlng, popupContent) => {
+//adds marker and popup to map 
+const marker = (latlng, popupContent) => {
+    //removes a marker if it already exists 
     if (currentMarker) {
-        map.removeLayer(currentMarker);
-    }
+        map.removeLayer(currentMarker); }
+    
+    //adds marker and binds popup with content 
     currentMarker = L.marker(latlng).addTo(map)
         .bindPopup(popupContent)
         .openPopup();
 
+    //removes marker when popup is closed 
     currentMarker.on('popupclose', () => {
         map.removeLayer(currentMarker);
-        currentMarker = null;
-    });
-};
+        currentMarker = null; }); };
 
+// map clicks
 const onMapClick = (e) => {
-    fetchWeatherData(e.latlng.lat, e.latlng.lng)
+    weatherData(e.latlng.lat, e.latlng.lng)
         .then(data => {
-            const popupContent = createPopupContent(data, e.latlng.lat, e.latlng.lng);
-            addMarkerToMap(e.latlng, popupContent);
-        })
+            //creates popup with marker on map
+            const popupContent = dataPopup(data, e.latlng.lat, e.latlng.lng);
+            marker (e.latlng, popupContent); })
         .catch(() => {
+            //display error if not able to get data 
             const errorPopupContent = '<b>Error:</b> Unable to fetch weather data';
-            addMarkerToMap(e.latlng, errorPopupContent);
-        });
-};
+            marker (e.latlng, errorPopupContent); }); };
 
+//adds onMapClick to maps click event 
 map.on('click', onMapClick);
 
+//triggered when you press enter in the search bar 
 document.getElementById('input').addEventListener('keydown', function (e) {
     if (e.key === 'Enter') {
         const searchTerm = this.value;
         if (searchTerm) {
+            
+            //geocoding API for countries 
             const geocodeApiUrl = `https://nominatim.openstreetmap.org/search?country=${searchTerm}&format=json&limit=1`;
 
             fetch(geocodeApiUrl)
                 .then(response => response.json())
                 .then(data => {
+                    
+                    //fits country to its coordinates when found 
                     if (data.length > 0) {
                         const countryCoords = [parseFloat(data[0].lat), parseFloat(data[0].lon)];
                         map.fitBounds([
                             [countryCoords[0] - 1, countryCoords[1] - 1],
                             [countryCoords[0] + 1, countryCoords[1] + 1]
-                        ]);
-                    } else {
+                        ]); } 
+                    else {
+                        //displays error if country not found 
                         const errorPopupContent = '<b>Error:</b> Country not found';
-                        addMarkerToMap(map.getCenter(), errorPopupContent);
-                    }
-                })
+                        marker (map.getCenter(), errorPopupContent); } })
+                
                 .catch(() => {
+                    //handles errors when getting data
                     const errorPopupContent = '<b>Error:</b> Unable to fetch data';
-                    addMarkerToMap(map.getCenter(), errorPopupContent);
-                });
+                    marker (map.getCenter(), errorPopupContent); });
+                //clear input value in input box 
+                this.value = ''; } 
+        } });
 
-            this.value = '';
-        }
-    }
-});
-
-showWelcomePopup();
+//show welcome popup for first time visitors 
+welcomePopup();
